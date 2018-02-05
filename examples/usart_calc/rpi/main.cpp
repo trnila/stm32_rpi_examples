6351acad54dd3f02ae6c36670dd1e2cdc60464d5
Daniel Trnka <daniel.trnka@gmail.com>
2018-02-05 23:17:47 +0100

    usart_calc: send quit signal to the queue

--- examples/usart_calc/rpi/main.cpp
+++ examples/usart_calc/rpi/main.cpp
@@ -7,10 +7,15 @@
 #include <termios.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include "serial.h"
 #include "fixedqueue.h"
 
-FixedQueue<std::pair<int, int>> queue(10);
+struct Item {
+	bool end;
+	std::pair<int, int> nums;
+};
+FixedQueue<Item> queue(10);
 
 void send(Serial &serial) {
 	char buffer[128];
@@ -19,13 +24,20 @@ void send(Serial &serial) {
 		int a = rand() % 150;
 		int b = rand() % 150;
 
-		queue.push(std::make_pair(a, b));
+		Item item;
+		item.end = false;
+		item.nums = std::make_pair(a, b);
+		queue.push(item);
 
 		snprintf(buffer, sizeof(buffer), "%d + %d\n", a, b);
 		serial.write(buffer, strlen(buffer));
 
 		printf("[SENT] %d + %d\n", a, b);
 	}
+
+	Item item;
+	item.end = true;
+	queue.push(item);
 }
 
 int main() {
@@ -38,19 +50,29 @@ int main() {
 	t.detach();
 
 	char buffer[128];
+	int ret = 0;
 	for(;;) {
+
+		Item item = queue.get();
+		if(item.end) {
+			break;
+		}
 		serial.readline(buffer, sizeof(buffer));
 
 		int a, b;
-		std::tie(a, b) = queue.get();
+		std::tie(a, b) = item.nums;
 
 		int result = 0;
 		if(sscanf(buffer, "%d", &result) != 1) {
 			printf("[ERR]  Parse error '%s'\n", buffer);
+			ret = 1;
 		} else if(a + b != result) {
 			printf("[ERR]  Calculation error %d-%d %d want %d\n", a, b, result, a+b);
+			ret = 1;
 		} else {
 			printf("[OK]   %d+%d=%d\n", a, b, result);
 		}
 	}
+
+	return ret;
 }
